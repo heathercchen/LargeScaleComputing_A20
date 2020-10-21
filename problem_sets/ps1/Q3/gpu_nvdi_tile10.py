7c82c359b8b926fee52dd155a61a93b822d9aa5c
Heather Chen <heatherchen@promote.cache-dns.local>
2020-10-21 20:26:45 +0800

    Update Question 3 materials

---
+++ problem_sets/ps1/Q3/gpu_nvdi_tile10.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.array as cl_array
+import pyopencl.tools as cltools
+from pyopencl.elementwise import ElementwiseKernel
+import matplotlib.pyplot as plt
+import time
+import rasterio
+
+def sim_gpu_nvdi():
+  band4 = rasterio.open('LC08_B4.tif') #red
+  band5 = rasterio.open('LC08_B5.tif') #nir
+
+  #Convert nit and red objects to float64 arrays
+  red = band4.read(1).astype('float64')
+  nir = band5.read(1).astype('float64')
+
+  #Tile the arrays 10 times
+  red = np.tile(red, 10)
+  nir = np.tile(nir, 10)
+
+  #Get the computation time using original serial solution
+  t0_s = time.time()
+  nvdi_s = (nir - red) / (nir + red)
+  final_time_s = time.time()
+  serial_time = final_time_s - t0_s
+
+  #Plot the graph using serial solution
+  plt.imsave('ps_q3_serial_tile10.png', nvdi_s)
+
+  #Get the computation time using gpu
+  #Set up OpenCL context and command queue
+  t0_g = time.time()
+  ctx = cl.create_some_context()
+  queue = cl.CommandQueue(ctx) 
+
+  red_dev = cl_array.to_device(queue, red)
+  nir_dev = cl_array.to_device(queue, nir)
+
+  nvdi_comb = ElementwiseKernel(ctx,"double *x, double *y, double *res","res[i] = (x[i] - y[i]) / (x[i] + y[i])")
+
+  res_gpu = cl.array.empty_like(nir_dev)
+  nvdi_comb(nir_dev, red_dev, res_gpu)
+  nvdi_gpu = res_gpu.get()
+  
+  final_time_g = time.time()
+  gpu_time = final_time_g - t0_g
+
+  #Plot the graph using gpu solution (to prove that they are the same)
+  plt.imsave('ps_q3_gpu_tile10.png', nvdi_gpu)
+
+  #Report the time for serial solution and gpu
+  print('The time using serial solution (10 times): {0:.4f} seconds'.format(serial_time))
+  print('The time using gpu (10 times): {0:.4f} seconds'.format(gpu_time))
+
+def main():
+  sim_gpu_nvdi()
+
+if __name__ == '__main__':
+  main()
